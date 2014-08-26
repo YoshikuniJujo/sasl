7e255bd35a51d1ed7b4a8ef74671893f75f9a23b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-26 09:12:24 +0900

    #18 add initSaslState

--- src/Network/Sasl.hs
+++ src/Network/Sasl.hs
@@ -18,6 +18,7 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 
 class SaslState s where
+	initSaslState :: s
 	getSaslState :: s -> [(BS.ByteString, BS.ByteString)]
 	putSaslState :: [(BS.ByteString, BS.ByteString)] -> s -> s
 
